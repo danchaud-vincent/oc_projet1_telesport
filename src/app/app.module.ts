7253426a4d08a851a2a3a7fde9d9c9ffbc5fae5d
danchaud-vincent <vdanchaud@gmail.com>
2025-04-14 17:46:05 +0200

    add BrowserAnimationsModule for lineChart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
@@ -13,7 +15,8 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
     BrowserModule, 
     AppRoutingModule, 
     HttpClientModule,
-    NgxChartsModule
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
