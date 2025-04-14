2a14e096004fd64365168cf7e3acff3d7a1c15b2
danchaud-vincent <vdanchaud@gmail.com>
2025-04-14 17:47:44 +0200

    update chart options of the linechart component

--- src/app/core/components/line-chart/line-chart.component.ts
+++ src/app/core/components/line-chart/line-chart.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-line-chart',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxChartsModule
+  ],
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.scss'
 })
 export class LineChartComponent {
+
+  @Input() chartData: any[] = [];
+  @Input() view: [number, number] = [700, 400];
+  chartOptions = {
+    showLegend: true,
+    showLabels: true,
+    animations: true,
+    xAxis: true,
+    yAxis: true,
+    showYAxisLabel: true,
+    showXAxisLabel: true,
+    xAxisLabel: 'Année',
+    yAxisLabel: 'Médailles',
+  };
 
 }
