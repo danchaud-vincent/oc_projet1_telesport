37a4ee59e9041866dcb5c817d44aa85cd6da7649
danchaud-vincent <vdanchaud@gmail.com>
2025-04-14 16:18:41 +0200

    get the olympic detail with the route and display the values in html

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,21 +1,30 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
 export class DetailComponent implements OnInit {
 
+  public olympic$!: Observable<Olympic>;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(
+    private olympicService: OlympicService,
+    private route: ActivatedRoute){}
 
   ngOnInit(): void {
-    const countryName = this.route.snapshot.params["name"];
-    console.log(countryName)
+    const countryName: string = this.route.snapshot.params["name"].trim();
+    this.olympic$ = this.olympicService.getOlympicByName(countryName);
   }
 
 }
