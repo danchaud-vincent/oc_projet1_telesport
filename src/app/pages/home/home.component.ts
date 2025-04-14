86123783ad9b8905e8f107556d6ff4296f596a07
danchaud-vincent <vdanchaud@gmail.com>
2025-04-14 11:02:47 +0200

    create a preparedChartData function to format the data for the pieChart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,47 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
+import { PieChartComponent } from 'src/app/core/components/pie-chart/pie-chart.component';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [
+    PieChartComponent,
+    AsyncPipe
+  ], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[]> = of([]);
+  public chartData$: Observable<any[]> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.chartData$= this.olympicService.getOlympics().pipe(
+      map(olympics => this.preparedDataForPieChart(olympics))
+    );
+
+    console.log(this.chartData$)
   }
+
+  preparedDataForPieChart(olympicsData: Olympic[]): any[]{
+
+    console.log(olympicsData)
+    const chartData = olympicsData.map(olympic => {
+      return {
+        name: olympic.country,
+        label: olympic.country,
+        value: (olympic.participations || []).reduce( (sum, p) => sum + p.medalsCount ,0)
+      }
+    })
+
+    return chartData
+  }
+
+
 }
